1c5b6715b9f6ebdefe334771b71dcc87eb2f05ef
fonkwe <williajeni@gmail.com>
2022-05-19 04:32:38 -0500

    code don spoil

---
+++ function splitify(str).js
@@ -0,0 +1,9 @@
+function splitify(str) {
+    // Only change code below this line
+  
+   let Array = str.split(/[\-]/g);
+   
+    // Only change code above this line
+  }
+  
+  console.log(splitify("Hello World,I-am code"));
